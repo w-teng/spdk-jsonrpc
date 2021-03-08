1eac35600e8594fa00b8f8c3ac5fcbf035dbcdbb
shenmu.wy <shenmu.wy@antfin.com>
2021-03-08 19:51:13 +0800

    add rpc methods

--- pkg/client/spdk.go
+++ pkg/client/spdk.go
@@ -2,12 +2,31 @@ package client
 
 import "encoding/json"
 
+var (
+	_ SPDKClientIface = &SPDK{}
+)
+
 type SPDKClientIface interface {
 	GetRawClient() JsonRpcClientIface
 	// RpcGetMethods rpc_get_methods
 	RpcGetMethods() ([]string, error)
-	// BdevAioCreate bdev_aio_create
+
+	// nvmf_create_transport
+	NVMFCreateTransport(req NVMFCreateTransportReq) (result bool, err error)
+
+	// BdevAioCreate bdev_aio_create, return the name of bdev
 	BdevAioCreate(req BdevAioCreateReq) (name string, err error)
+	// bdev_lvol_create_lvstore
+	BdevLVolCreateLVStore(req BdevLVolCreateLVStoreReq) (uuid string, err error)
+	// bdev_lvol_create
+	BdevLVolCreate(req BdevLVolCreateReq) (uuid string, err error)
+
+	// nvmf_create_subsystem
+	NVMFCreateSubsystem(req NVMFCreateSubsystemReq) (result bool, err error)
+	// nvmf_subsystem_add_ns
+	NVMFSubsystemAddNS(req NVMFSubsystemAddNSReq) (nsID int, err error)
+	// nvmf_subsystem_add_listener
+	NVMFSubsystemAddListener(req NVMFSubsystemAddListenerReq) (result bool, err error)
 }
 
 type SPDK struct {
@@ -40,5 +59,62 @@ func (s *SPDK) BdevAioCreate(req BdevAioCreateReq) (name string, err error) {
 		return
 	}
 	err = json.Unmarshal(result, &name)
+	return
+}
+
+func (s *SPDK) NVMFCreateTransport(req NVMFCreateTransportReq) (res bool, err error) {
+	result, err := s.rawCli.Call("nvmf_create_transport", req)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(result, &res)
+	return
+}
+
+func (s *SPDK) BdevLVolCreateLVStore(req BdevLVolCreateLVStoreReq) (uuid string, err error) {
+	result, err := s.rawCli.Call("bdev_lvol_create_lvstore", req)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(result, &uuid)
+	return
+}
+
+// bdev_lvol_create
+func (s *SPDK) BdevLVolCreate(req BdevLVolCreateReq) (uuid string, err error) {
+	result, err := s.rawCli.Call("bdev_lvol_create", req)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(result, &uuid)
+	return
+}
+
+func (s *SPDK) NVMFCreateSubsystem(req NVMFCreateSubsystemReq) (res bool, err error) {
+	result, err := s.rawCli.Call("nvmf_create_subsystem", req)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(result, &res)
+	return
+}
+
+// nvmf_subsystem_add_ns
+func (s *SPDK) NVMFSubsystemAddNS(req NVMFSubsystemAddNSReq) (nsID int, err error) {
+	result, err := s.rawCli.Call("nvmf_create_transport", req)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(result, &nsID)
+	return
+}
+
+// nvmf_subsystem_add_listener
+func (s *SPDK) NVMFSubsystemAddListener(req NVMFSubsystemAddListenerReq) (res bool, err error) {
+	result, err := s.rawCli.Call("nvmf_create_transport", req)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(result, &res)
 	return
 }
--- pkg/client/types.go
+++ pkg/client/types.go
@@ -7,7 +7,22 @@ import (
 
 const (
 	JSONRPCVersion = "2.0"
+
+	ClearMethodNone        ClearMethod = "none"
+	ClearMethodUnmap       ClearMethod = "unmap"
+	ClearMethodWriteZeroes ClearMethod = "write_zeroes"
+
+	// "IPv4", "IPv6", "IB", or "FC"
+	AddrFamilyIPv4 AddressFamily = "IPv4"
+	AddrFamilyIPv6 AddressFamily = "IPv6"
+	AddrFamilyIB   AddressFamily = "IB"
+	AddrFamilyFC   AddressFamily = "FC"
+
+	TransportTypeRDMA = "RDMA"
 )
+
+type ClearMethod string
+type AddressFamily string
 
 type RPCRequest struct {
 	RPCVersion string      `json:"jsonrpc"`
@@ -34,7 +49,113 @@ func (e RPCError) Error() string {
 
 // BdevAioCreateReq bdev_aio_create
 type BdevAioCreateReq struct {
-	BdevName  string `json:"name"`
-	FileName  string `json:"filename"`
-	BlockSize int    `json:"block_size,omitempty"`
+	// required
+	BdevName string `json:"name"`
+	FileName string `json:"filename"`
+	// optional
+	BlockSize int `json:"block_size,omitempty"`
+}
+
+type BdevLVolCreateLVStoreReq struct {
+	// required
+	BdevName string `json:"bdev_name"`
+	LvsName  string `json:"lvs_name"`
+	// optional
+	// Cluster size of the logical volume store in bytes
+	ClusterSize int         `json:"cluster_sz,omitempty"`
+	ClearMethod ClearMethod `json:"clear_method,omitempty"`
+}
+
+type BdevLVolCreateReq struct {
+	// required
+	LVolName string `json:"lvol_name"`
+	// Desired size of logical volume in bytes; Size will be rounded up to a multiple of cluster size. Either uuid or lvs_name must be specified, but not both. lvol_name will be used in the alias of the created logical volume.
+	Size int `json:"size"`
+
+	// optional
+	ThinProvision bool        `json:"thin_provision,omitempty"`
+	UUID          string      `json:"uuid,omitempty"`
+	LvsName       string      `json:"lvs_name,omitempty"`
+	ClearMethod   ClearMethod `json:"clear_method,omitempty"`
+}
+
+type NVMFCreateSubsystemReq struct {
+	// required
+	NQN string `json:"nqn"`
+
+	// optional
+	// Parent NVMe-oF target name.
+	TargetName string `json:"tgt_name,omitempty"`
+	// Serial number of virtual controller
+	SerialNumber string `json:"serial_number,omitempty"`
+	// Model number of virtual controller
+	ModelNumber   string `json:"model_number,omitempty"`
+	MaxNamespaces int    `json:"max_namespaces,omitempty"`
+	AllowAnyHost  bool   `json:"allow_any_host,omitempty"`
+	ANAReporting  bool   `json:"ana_reporting,omitempty"`
+}
+
+type NVMFSubsystemAddNSReq struct {
+	// required
+	NQN       string    `json:"nqn"`
+	Namespace Namespace `json:"namespace"`
+
+	// optional
+	TargetName string `json:"tgt_name,omitempty"`
+}
+
+type Namespace struct {
+	// req
+	BdevName string `json:"bdev_name"`
+
+	// opt
+	NSID     int    `json:"nsid,omitempty"`
+	NGUID    string `json:"nguid,omitempty"`
+	EUI64    string `json:"eui64,omitempty"`
+	UUID     string `json:"uuid,omitempty"`
+	PtplFile string `json:"ptpl_file,omitempty"`
+}
+
+type ListenAddress struct {
+	// required
+	TrType string        `json:"trtype"` // RDMA
+	AdrFam AddressFamily `json:"adrfam"`
+	TrAddr string        `json:"traddr"`
+	// opt
+	TrSvcID string `json:"trsvcid,omitempty"`
+}
+
+type NVMFSubsystemAddListenerReq struct {
+	// required
+	NQN string `json:"nqn"`
+	// opt
+	TargetName    string        `json:"tgt_name,omitempty"`
+	ListenAddress ListenAddress `json:"listen_address,omitempty"`
+}
+
+type NVMFCreateTransportReq struct {
+	// required
+	TrType string `json:"trtype"`
+
+	// optional
+	TargetName          string `json:"tgt_name,omitempty"`
+	MaxQueueDepth       int    `json:"max_queue_depth,omitempty"`
+	MaxQPairsPerCtrlr   int    `json:"max_qpairs_per_ctrlr,omitempty"`
+	MaxIOQPairsPerCtrlr int    `json:"max_io_qpairs_per_ctrlr,omitempty"`
+	InCapsuleDataSize   int    `json:"in_capsule_data_size,omitempty"`
+	MaxIOSize           int    `json:"max_io_size,omitempty"`
+	IOUnitSize          int    `json:"io_unit_size,omitempty"`
+	MaxAQDepth          int    `json:"max_aq_depth,omitempty"`
+	NumSharedBuffers    int    `json:"num_shared_buffers,omitempty"`
+	BufCacheSize        int    `json:"buf_cache_size,omitempty"`
+	NumCQE              int    `json:"num_cqe,omitempty"`
+	MaxSRQDepth         int    `json:"max_srq_depth,omitempty"`
+	SockPriority        int    `json:"sock_priority,omitempty"`
+	AcceptorBacklog     int    `json:"acceptor_backlog,omitempty"`
+	AbortTimeoutSec     int    `json:"abort_timeout_sec,omitempty"`
+
+	NoWrBatching     bool `json:"no_wr_batching,omitempty"`
+	DifInsertOrStrip bool `json:"dif_insert_or_strip,omitempty"`
+	C2hSuccess       bool `json:"c2h_success,omitempty"`
+	NoSRQ            bool `json:"no_srq,omitempty"`
 }
